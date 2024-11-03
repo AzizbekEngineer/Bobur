c48667d00bcd75368b9dd8b54e0cfa4e6bc4ff41
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-11-03 17:30:47 +0500

    add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
+import Video from "../../components/vidio/Video";
+import Global from "../../components/global/Global";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Global />
+      <Video />
     </div>
   );
 };
