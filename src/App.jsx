6c831c09e4c7ab7e393d83738ffb2d2682ec23fe
AzizbekEngineer <azizbekengineer@gmail.com>
2024-11-03 17:43:04 +0500

    about

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="home" element={<Home />} />
+          <Route index path="home" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="blog" element={<Blog />} />
           <Route path="devon" element={<Devon />} />
